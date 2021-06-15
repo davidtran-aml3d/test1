36fee06b0eab0dc505c7f8a0c45cda392742dbb2
David Tran <David.tran@aml3d.com>
2021-06-15 12:23:02 +0930

    test2

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World from AML3d !\n";
+    std::cout << "Hello World from AML3D !!!!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
